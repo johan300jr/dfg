7e7531ac7bd683dfd79b2552cafe548b1df82570
johan300jr <109961781+johan300jr@users.noreply.github.com>
2022-07-25 07:11:38 -0500

    Add files via upload

---
+++ Nueva carpeta (2)/index.php
@@ -0,0 +1,28 @@
+¿php 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <nav>
+        
+        </nav>
+    </header>
+
+    <article>
+        <section>
+        
+
+        
+        </section>
+    </article>
+
+</body>
+</html>
+?
